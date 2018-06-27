447f19178720ae4c70dce9cd4dd4c008b67db51b
Ethan Jon <e10jc@users.noreply.github.com>
2018-06-27 14:12:11 -0700

    react-markdown

--- src/components/guide.tsx
+++ src/components/guide.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import * as Markdown from 'react-markdown'
 import {Box, Caps, Container, Flex, Heading, Text} from 'rebass'
 
 import {GSection} from '../pages/guide'
@@ -15,7 +16,7 @@ class Guide extends React.Component<Props> {
       <Box>
         <Container maxWidth={900}>
           <Flex flexWrap='wrap' mx={-3} my={3}>
-            <Box px={3} width={[1, 1 / 4]}>
+            <Box px={3} width={[1, 1, 1 / 4]}>
               <NavContainer>
                 <Box px={3} py={4}>
                   <Caps>Timeline</Caps>
@@ -28,13 +29,13 @@ class Guide extends React.Component<Props> {
               </NavContainer>
             </Box>
 
-            <Box px={3} width={[1, 3 / 4]}>
+            <Box px={3} width={[1, 1, 3 / 4]}>
               {sections.map(({node: {blocks, id}}) => (
                 <Box key={id}>
                   {blocks.map(({body: {body}, id, title}) => (
                     <Box key={id} mb={3}>
                       <Heading fontSize={4} mb={2}>{title}</Heading>
-                      <Text mb={2}>{body}</Text>
+                      <Markdown source={body} />
                     </Box>
                   ))}
                 </Box>
