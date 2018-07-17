fe13f1c074c95cb55a78335c105b37a52aa1695e
Ethan Jon <e10jc@users.noreply.github.com>
2018-07-17 06:07:50 -0700

    mobile menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 import Link from 'gatsby-link'
 import * as React from 'react'
-import {Box, Flex, Heading, Text} from 'rebass'
+import {Box, Drawer, Flex, Heading, Text} from 'rebass'
 
 import {AuthContext} from '../layouts/index'
 import Div from './div'
@@ -9,9 +9,22 @@ interface Props {
   canViewGuide: boolean,
 }
 
+interface State {
+  isMobileMenuOpen: boolean,
+}
+
 class Header extends React.Component<Props> {
+  state = {
+    isMobileMenuOpen: false,
+  }
+
   render () {
     const {canViewGuide} = this.props
+
+    const links = [
+      [canViewGuide ? '/guide/' : '/quiz/', 'Guide'],
+      ['/about/', 'About'],
+    ]
 
     return (
       <Box bg='purple' className='sans-2'>
@@ -23,17 +36,47 @@ class Header extends React.Component<Props> {
           </Box>
           <Div display={['none', 'block']}>
             <Flex>
-              <Link to={canViewGuide ? '/guide/' : '/quiz/'}>
-                <Text color='white' p={3}>Guide</Text>
-              </Link>
-              <Link to='/about/'>
-                <Text color='white' p={3}>About</Text>
-              </Link>
+              {links.map(([to, text]) => (
+                <Link key={to} to={to}>
+                  <Text color='white' p={3}>{text}</Text>
+                </Link>
+              ))}
             </Flex>
           </Div>
+          <Div display={['block', 'none']}>
+            <a href='javascript:void(0)' onClick={this.handleMobileMenuOpen}>
+              <Text color='white' p={3}>Menu</Text>
+            </a>
+          </Div>
         </Flex>
+
+        <Drawer
+          bg='darkPurple'
+          color='white'
+          open={this.state.isMobileMenuOpen}
+          p={3}
+          side='right'
+          zIndex={1}
+        >
+          <a href='javascript:void(0)' onClick={this.handleMobileMenuClose}>
+            <Text color='white' p={3}>Close</Text>
+          </a>
+          {links.map(([to, text]) => (
+            <Link key={to} to={to}>
+              <Text color='white' p={3}>{text}</Text>
+            </Link>
+          ))}
+        </Drawer>
       </Box>
     )
+  }
+
+  handleMobileMenuClose = () => {
+    this.setState({...this.state, isMobileMenuOpen: false})
+  }
+
+  handleMobileMenuOpen = () => {
+    this.setState({...this.state, isMobileMenuOpen: true})
   }
 }
 
